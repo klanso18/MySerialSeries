0d607e9430ebccd1ba70188f5bb6b01f3bc0fa78
Mary <regentmarysapro@gmail>
2022-04-21 22:49:27 +0200

    add route category add category_html_twig add CategoryController.php add title add carrousel add clic picture add false content add swip

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,2 +1,11 @@
-<?php  
+<?php
 
+namespace App\Controller;
+
+class CategoryController extends AbstractController
+{
+    public function index(): string
+    {
+        return $this->twig->render('category/category.html.twig');
+    }
+}
