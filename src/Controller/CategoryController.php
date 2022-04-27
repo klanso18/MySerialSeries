c9f3f22a1a33999ea896163512b706d0e9067aef
H Rasamy <rasamherve@gmail.com>
2022-04-27 13:00:54 +0200

    update

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,10 +2,21 @@
 
 namespace App\Controller;
 
+use App\Model\CategoryManager;
+use App\Model\SerieManager;
+
 class CategoryController extends AbstractController
 {
     public function index(): string
     {
-        return $this->twig->render('category/category.html.twig');
+
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll();
+        $serieManager = new SerieManager();
+        $series = $serieManager->selectAll();
+        return $this->twig->render('category/category.html.twig', [
+            'categories' => $categories,
+            'series' => $series
+        ]);
     }
 }
