9297e386e86900307f8ed3eb2b86f8f88f76c3fb
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-04-27 16:30:05 +0200

    page add created and edited

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -8,4 +8,9 @@ class SerieController extends AbstractController
     {
         return $this->twig->render('Serie/index.html.twig');
     }
+
+    public function add(): string
+    {
+        return $this->twig->render('Serie/add.html.twig');
+    }
 }
