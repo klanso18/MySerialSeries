5623aec904ffb4d3aa4124a62c693c642883bf16
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-04-28 15:11:45 +0200

    editor page and controller updated

--- database.sql
+++ database.sql
@@ -1,63 +1,165 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: serial_series
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `simple-mvc`
+-- Table structure for table `category`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `item`
+-- Dumping data for table `category`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES 
+(1, 'Japanese Animation'),
+(2, 'Horror'),
+(3, 'Comedy'),
+(4, 'Thriller');
+
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Contenu de la table `item`
+-- Table structure for table `serie`
 --
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+DROP TABLE IF EXISTS `serie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `serie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `number_serie` int DEFAULT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  `description` text, 
+  `category_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_serie_category_idx` (`category_id`),
+  CONSTRAINT `fk_serie_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Index pour les tables exportées
+-- Dumping data for table `serie`
 --
 
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `serie` WRITE;
+/*!40000 ALTER TABLE `serie` DISABLE KEYS */;
+INSERT INTO `serie` VALUES 
+(1, 'Jijutsu kaizen0', 1, 'Jijutsu_kaizen0.jpg', 'Desc',1),
+(2, 'Jijutsu kaizen', 2, 'Jijutsu_kaizen.jpg', 'Desc',1),
+(3, 'Demon Slayers', 3, 'Demonslayer1.jpg', 'Desc',1),
+(4, 'Attack on Titans', 4, 'attack_on_titans1.jpg', 'Desc',1),
+(5, 'The walking dead', 5, 'walkingdead.jpeg', 'Desc',1),
+(6, 'Dark', 6, 'dark.jpeg', 'Desc',1),
+(7, 'Game of Thrones', 7, 'goat.jpeg', 'Desc',1),
+(8, 'Stranger Things', 8, 'strangerThings.jpeg', 'Desc',1),
+(9, 'Rick and Morty', 9, 'rick_morty.jpg', 'Desc',1),
+(10, 'Sex Education', 10, 'sex_education.jpg', 'Desc',1),
+(11, 'Shameless', 11, 'shameless.jpg', 'Desc',1),
+(12, 'Peacemaker', 12, 'peacemakerS1.jpg', 'Desc',1),
+(13, 'Upload', 13, 'upload.jpg', 'Desc',1),
+(14, 'Breaking Bad', 14, 'breakingbad.jpg', 'Desc',1),
+(15, 'Dexter', 15, 'dexter.jpeg', 'Desc',1),
+(16, 'Ozark', 16, 'ozark.jpeg', 'Desc',1),
+(17, 'You', 17, 'you.jpeg', 'Desc',1);
+
+
+/*!40000 ALTER TABLE `serie` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `birthday` datetime DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 --
--- AUTO_INCREMENT pour la table `item`
+-- Dumping data for table `user`
 --
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seen`
+--
+
+DROP TABLE IF EXISTS `seen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seen` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `is_completed` tinyint DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `serie_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_seen_serie_idx` (`serie_id`),
+  KEY `fk_seen_user_idx` (`user_id`),
+  CONSTRAINT `fk_seen_serie` FOREIGN KEY (`serie_id`) REFERENCES `serie` (`id`),
+  CONSTRAINT `fk_seen_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seen`
+--
+
+LOCK TABLES `seen` WRITE;
+/*!40000 ALTER TABLE `seen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-21 23:39:26
+
--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Model\SerieManager;
+
 class SerieController extends AbstractController
 {
     public function index(): string
@@ -9,8 +11,21 @@ class SerieController extends AbstractController
         return $this->twig->render('Serie/index.html.twig');
     }
 
-    public function add(): string
+    public function add(): ?string
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $serie = array_map('trim', $_POST);
+
+            $fileName = $_FILES['image']['name'];
+            $uploadFile = __DIR__ . '/../../public/uploads/' . $fileName;
+            if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile)) {
+                $serieManager = new SerieManager();
+                $serie['image'] = $fileName;
+                $id = $serieManager->insert($serie);
+                header('Location:/serie?id=' . $id);
+                return null;
+            }
+        }
         return $this->twig->render('Serie/add.html.twig');
     }
 }
