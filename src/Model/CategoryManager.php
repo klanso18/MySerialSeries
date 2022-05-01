f3dad640fa44a9bd9240b4b0d73dba1216b1121b
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-05-01 23:04:22 +0200

    categoryManager updated

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -5,4 +5,15 @@ namespace App\Model;
 class CategoryManager extends AbstractManager
 {
     public const TABLE = 'category';
+
+    public function selectDistinctAll(string $orderBy = '', string $direction = 'ASC'): array
+    {
+        $query = 'SELECT DISTINCT c.id, c.name FROM ' . static::TABLE
+        . ' AS c JOIN ' . SerieManager::TABLE . ' AS i ON i.category_id=c.id ';
+        if ($orderBy) {
+            $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
+        }
+
+        return $this->pdo->query($query)->fetchAll();
+    }
 }
